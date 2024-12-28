ebb8d5597b903b57ec4057edc963cd84a817de20
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-12-28 14:57:00 +0900

    rails db:migrate

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,12 @@
 class Organization < ApplicationRecord
 end
+
+# == Schema Information
+#
+# Table name: organizations
+#
+#  id         :integer          not null, primary key
+#  role       :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
