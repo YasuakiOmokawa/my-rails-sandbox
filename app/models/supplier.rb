4b08a58f706a124da312c2ed49d851e19017d0c8
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-01-25 10:00:00 +0900

    delegation, alias, and so on

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,9 +1,19 @@
 class Supplier < ApplicationRecord
   has_one :account
 
+  delegate :name, to: :account, prefix: true
+
+  alias :a_name :account_name
+
   def identical_name
     "#{id}_#{name}"
   end
+
+  def a_name2
+    account.name
+  end
+
+  define_method(:a_name3, instance_method(:account_name))
 end
 
 # == Schema Information
